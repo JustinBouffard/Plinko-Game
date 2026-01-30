bdc2415ef23cae0248737edd80473c35f6dda642
JustinBouffard <bouffardjustin@gmail.com>
2026-01-30 10:55:53 -0500

    Fine-tuning the main scene

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -4,14 +4,6 @@ extends Node2D
 
 var score := 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_plus_point_body_entered(body: Node2D) -> void:
 	score += 3
 	$ScoreLabel.text = "Score: " + str(score)
